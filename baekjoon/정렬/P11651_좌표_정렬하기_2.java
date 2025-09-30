7f9fdc90a2e3978b4b9dd5706a10a4c27295e08e
gunwooknam2023 <taeme0912@naver.com>
2025-09-30 22:00:05 +0900

    Backjoon +4, Programmers +0

---
+++ baekjoon/정렬/P11651_좌표_정렬하기_2.java
@@ -0,0 +1,36 @@
+package 정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P11651_좌표_정렬하기_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int arr[][] = new int[N][2];
+
+        for(int i=0; i<N; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if(a[1] == b[1]){
+                return a[0] - b[0];
+            } else{
+                return a[1] - b[1];
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int a[] : arr){
+            sb.append(a[0]).append(' ').append(a[1]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
---
+++ baekjoon/정렬/P2309_일곱_난쟁이.java
@@ -0,0 +1,54 @@
+package 정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class P2309_일곱_난쟁이 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int arrN[] = new int[9];
+        for(int i=0; i<9; i++){
+            arrN[i] = Integer.parseInt(br.readLine());
+        }
+
+        int sum = 0;
+        for(int a : arrN){
+            sum += a;
+        }
+
+        int fake1 = 0;
+        int fake2 = 0;
+
+        for(int i=0; i<9; i++){
+            for(int j=i+1; j<9; j++){
+                if(sum - (arrN[i] + arrN[j]) == 100){
+                    fake1 = i;
+                    fake2 = j;
+                    break;
+                }
+            }
+        }
+
+        int[] result = new int[7];
+        int idx = 0;
+        for(int i=0; i<9; i++){
+            if(i != fake1 && i != fake2){
+                result[idx] = arrN[i];
+                idx++;
+            }
+        }
+
+        Arrays.sort(result);
+
+        StringBuilder sb = new StringBuilder();
+        for(int a : result){
+            sb.append(a).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
