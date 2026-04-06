3ec4fe945a7d8f20b057e391a6e0303c6804ca1d
gunwooknam2023 <taeme0912@naver.com>
2026-04-06 22:27:51 +0900

    Backjoon +2, Programmers +0

---
+++ baekjoon/반복문/브론즈/P11022_A더하기B빼기8.java
@@ -0,0 +1,21 @@
+package 반복문.브론즈;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11022_A더하기B빼기8 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int i=1; i<=t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            System.out.println("Case #" + i + ": " + a + " + " + b + " = " + (a+b));
+        }
+    }
+}
