3b609c71501d5ecc6776e8beb5f633f4d2ec2c74
gunwooknam2023 <taeme0912@naver.com>
2026-05-13 00:28:24 +0900

    codetree +4

---
+++ codetree/trail0/ch1/P_2줄_출력.java
@@ -0,0 +1,8 @@
+package trail0.ch1;
+
+public class P_2줄_출력 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("World");
+    }
+}
---
+++ codetree/trail0/ch1/P_문자_변경하기.java
@@ -0,0 +1,10 @@
+package trail0.ch1;
+
+public class P_문자_변경하기 {
+    public static void main(String[] args) {
+        char a = 'C';
+        a = 'T';
+
+        System.out.println(a);
+    }
+}
