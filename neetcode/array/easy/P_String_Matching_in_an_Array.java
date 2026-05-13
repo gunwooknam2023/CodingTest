56d4fce56bad96c7612cda93bcbb9d6680db7f96
gunwooknam2023 <taeme0912@naver.com>
2026-05-14 00:12:07 +0900

    neetcode +3

---
+++ neetcode/array/easy/P_String_Matching_in_an_Array.java
@@ -0,0 +1,25 @@
+package array.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P_String_Matching_in_an_Array {
+    class Solution {
+        public List<String> stringMatching(String[] words) {
+            List<String> list = new ArrayList<>();
+
+            for (int i=0; i<words.length; i++) {
+                for (int j=0; j<words.length; j++) {
+                    if (i == j) continue;
+
+                    if (words[j].contains(words[i])) {
+                        list.add(words[i]);
+                        break;
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
---
+++ neetcode/array/easy/P_Unique_Email_Addresses.java
@@ -0,0 +1,30 @@
+package array.easy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class P_Unique_Email_Addresses {
+    class Solution {
+        public int numUniqueEmails(String[] emails) {
+            Set<String> uniqueEmails = new HashSet<>();
+
+            for (String email : emails) {
+                String[] parts = email.split("@");
+                String localName = parts[0];
+                String domainName = parts[1];
+
+                if (localName.contains("+")) {
+                    String[] localParts = localName.split("\\+");
+                    localName = localParts[0];
+                }
+
+                localName = localName.replace(".", "");
+
+                uniqueEmails.add(localName + "@" + domainName);
+            }
+
+            return uniqueEmails.size();
+        }
+    }
+
+}
